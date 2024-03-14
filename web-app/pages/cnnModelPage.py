636402c5226a560b9e8d87f2a1b16157e902f55d
sahiltapu <sahilmishranayagarh@gmail.com>
2024-03-14 16:05:10 +0530

    added suggestion images section

--- web-app/pages/cnnModelPage.py
+++ web-app/pages/cnnModelPage.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import os
 from CNN_Predict import loadModel , predict_image_class_bytes
+from PIL import Image
+
 
 def is_image(uploaded_file):
     return uploaded_file.type.startswith("image/") if uploaded_file else False
@@ -47,10 +50,36 @@ def cnn_model():
             predicted_class = predict_image_class_bytes(model,image_path,300,300)
             class_name = ['basophil', 'eosinophil', 'erythroblast', 'ig', 'lymphocyte', 'monocyte', 'neutrophil', 'platelet'] 
             st.subheader(f'The predicted class is: {class_name[predicted_class]}')
+            
+def display_images_from_assets_folder(folder_path, cols, images_per_row):
+    row_count = 0
+    tcount = 0
+    for root, dirs, files in os.walk(folder_path):
+        tcount = 0
+        
+        for file in files:
+            if tcount == 0:
+                st.markdown(os.path.basename(root))
+                
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                tcount = 1
+                image_path = os.path.join(root, file)
+                with Image.open(image_path) as img:
+                    img.thumbnail((150, 150))
+                    cols[row_count % images_per_row].image(img, use_column_width=True)
+                row_count += 1
+                if row_count % images_per_row == 0:
+                    cols = st.columns(8)
 
 def main():
     st.set_page_config(layout="wide", page_title="Leukocyte Image Prediction")
     cnn_model()
+   # Display images from folders inside the assets folder
+    st.markdown("## Some suggestions from different types.")
+    asset_folder = "assets"
+    cols = st.columns(8)
+    images_per_row = 5
+    display_images_from_assets_folder(asset_folder , cols , images_per_row)
 
 if __name__ == "__main__":
     main()
